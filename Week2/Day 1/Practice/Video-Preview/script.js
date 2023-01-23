c4c2c1bc31129c32b31dfe8e32a54162a4ba9021
Ghaith Amaimia <ghaith.amaimia@gmail.com>
2023-01-23 20:17:36 +0100

    Video Preview

---
+++ Week2/Day 1/Practice/Video-Preview/script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+var element = document.getElementById("myVideo");
+
+function playVid(element) {
+element.play();
+}
+
+function pauseVid(element) {
+element.pause();
+}
