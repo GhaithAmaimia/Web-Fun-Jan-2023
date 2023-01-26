b32ad637d25d4f298a4f512b4cbf83f8c4cc1517
Ghaith Amaimia <ghaith.amaimia@gmail.com>
2023-01-26 13:19:56 +0100

    Liking likes

--- Week2/Day 2/Practice/Liking Likes/script.js
+++ Week2/Day 2/Practice/Liking Likes/script.js
@@ -1,7 +1,21 @@
-var count = 3;
-var countElement= document.querySelector(".likes");
+var count = 9;
+var countElement= document.querySelector("#likess");
 
-function addlike(){
+function addLike(){
     count++;
-    countElement.innerText = count +" like(s)";
+    countElement.innerText = count +" Like(s)";
+}
+var count1 = 12;
+var countElement1= document.querySelector("#likesss");
+
+function addLike1(){
+    count1++;
+    countElement1.innerText = count1 +" Like(s)";
+}
+var count2 = 9;
+var countElement2= document.querySelector("#likessss");
+
+function addLike2(){
+    count2++;
+    countElement2.innerText = count2 +" Like(s)";
 }
