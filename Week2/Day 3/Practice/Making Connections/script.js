b64e1ba30ba87feb9cc46ff0de2acd1ad172f33d
Ghaith Amaimia <ghaith.amaimia@gmail.com>
2023-01-26 11:40:06 +0100

    Making Connections

---
+++ Week2/Day 3/Practice/Making Connections/script.js
@@ -0,0 +1,31 @@
+function remove(){
+    document.querySelector('.todd').remove();
+    var RqstElement=document.querySelector('.two');
+    RqstElement.innerText--;
+    var cnxElement= document.querySelector('.five');
+    cnxElement.innerText++;
+}
+function remove1(){
+    document.querySelector('.todd').remove();
+    var RqstElement=document.querySelector('.two');
+    RqstElement.innerText--;
+}
+function remove2(){
+    document.querySelector('.phil').remove();
+    var RqstElement=document.querySelector('.two');
+    RqstElement.innerText--;
+    var cnxElement= document.querySelector('.five');
+    cnxElement.innerText++;
+}
+function remove3(){
+    document.querySelector('.phil').remove();
+    var RqstElement=document.querySelector('.two');
+    RqstElement.innerText--;
+}
+function ChangeName(){
+    document.querySelector('#jane').innerText='Ghaith Amaimia';
+}
+
+
+
+
